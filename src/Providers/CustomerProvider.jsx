4a51307a27c2ec674cda8e3aefb91ea11154a25b
Rahat Hossain <webmaaz@gmail.com>
2024-09-17 16:24:08 +0600

    check customer

--- src/Providers/CustomerProvider.jsx
+++ src/Providers/CustomerProvider.jsx
@@ -5,40 +5,60 @@ export const CustomerContext = createContext()
 
 const CustomerProvider = ({ children }) => {
   const [loading, setLoading] = useState(true)
-  const [customerData, setCustomerData] = useState(null)
+  const [customerData, setCustomerData] = useState(false)
 
   useEffect(() => {
-    const verifyCustomer = async () => {
-      const storedData = JSON.parse(localStorage.getItem('customerData'))
+    const token = localStorage.getItem('customerToken') ?? null;
 
-      if (storedData) {
-        try {
-          const response = await axios.get(
-            `${import.meta.env.VITE_BASE_URL}/api/v1/front/customer-check`,
-            {
-              headers: { Authorization: `Bearer ${storedData.token}` },
-            },
-          )
-          if (response.status === 200) setCustomerData(storedData)
-          else localStorage.removeItem('customerData')
-        } catch {
-          localStorage.removeItem('customerData')
+    if (token) {
+
+
+      axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/front/customer/customer-check`, {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
-      }
+      }).then(response => {
+        if (response.data.status === 200) {
+          setLoading(false)
+          setCustomerData(response.data)
+        } else {
+          setLoading(false)
+          setCustomerData(false)
+        }
+      }).catch(error => {
+        setLoading(false)
+      })
 
-      setLoading(false)
+      // axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/front/customer-check`).then(response => {
+
+      //   console.log(response,'success')
+      //   if(response.data.status === 200){
+      //     setCustomerData(true)
+      //   }else{
+      //     setCustomerData(false)
+      //   }
+      //   setLoading(false)
+      //   // if (response.status === 200) 
+      //   // else localStorage.removeItem('customerData')
+      // }).catch(error => {
+
+      //   console.log(response,'error')
+      //   setLoading(false)
+      //   setCustomerData(false)
+      // })
+
     }
 
-    verifyCustomer()
   }, [])
+
+  const data = {
+    loading,
+    customerData
+  }
 
   return (
     <CustomerContext.Provider
-      value={{
-        id: customerData?.customer?.id,
-        token: customerData?.token,
-        loading,
-      }}
+      value={data}
     >
       {children}
     </CustomerContext.Provider>
