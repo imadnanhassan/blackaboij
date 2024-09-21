4948393b1279fe70fc77217326154cb322a599a6
Dev-Hassan <abulhassan.dev@gmail.com>
2024-09-21 18:00:36 +0600

    date formate

--- src/hooks/useFormattedDate.js
+++ src/hooks/useFormattedDate.js
@@ -1,15 +1,11 @@
-import { useMemo } from 'react'
+
 
 // Custom hook for formatting a date
 export const useFormattedDate = isoDate => {
-  return useMemo(() => {
-    if (!isoDate) return ''
-
-    const date = new Date(isoDate)
-    return date.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    })
-  }, [isoDate])
+ const date = new Date(isoDate)
+ return date.toLocaleDateString('en-US', {
+   year: 'numeric',
+   month: 'long',
+   day: 'numeric',
+ })
 }
