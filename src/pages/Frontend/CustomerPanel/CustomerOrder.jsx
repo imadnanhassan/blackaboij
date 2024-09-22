646686c517eeb0bdb5133b92a824fa96e418a7d4
joySutradhor <joysutradhorcmt@gmail.com>
2024-09-22 21:30:10 +0600

    user order list customised table done

--- src/pages/Frontend/CustomerPanel/CustomerOrder.jsx
+++ src/pages/Frontend/CustomerPanel/CustomerOrder.jsx
@@ -44,32 +44,32 @@ export default function CustomerOrder() {
         <div className="flex flex-col">
           <div className="-m-1.5 overflow-x-auto">
             <div className=" min-w-full inline-block align-middle">
-              <div className="border overflow-hidden dark:border-neutral-700 dark:shadow-gray-900">
-                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                  <thead className="bg-gray-50 dark:bg-neutral-700">
-                    <tr>
+              <div className=" overflow-hidden dark:border-neutral-700 dark:shadow-gray-900">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700 ">
+                  <thead className="bg-gray-50 dark:bg-neutral-700  ">
+                    <tr className=''>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                        className="px-6 py-3 text-start text-xs md:text-sm text-gray-500 uppercase dark:text-neutral-400"
                       >
                         Date
                       </th>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                        className="px-6 py-3 text-start text-xs md:text-sm  text-gray-500 uppercase dark:text-neutral-400"
                       >
                         Amount
                       </th>
 
                       <th
                         scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                        className="px-6 py-3 text-start text-xs md:text-sm text-gray-500 uppercase dark:text-neutral-400"
                       >
                         Status
                       </th>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                        className="px-6 py-3 text-start text-xs md:text-sm  text-gray-500 uppercase dark:text-neutral-400"
                       >
                         Action
                       </th>
@@ -79,9 +79,9 @@ export default function CustomerOrder() {
                   {loading && isLoading ? (
                     <FaSpinner className="animate-spin" />
                   ) : (
-                    <tbody className="divide-y divide-neutral-700 ">
+                    <tbody className="divide-y divide-gray-200 ">
                       {orders?.map((order, index) => (
-                        <tr key={index}>
+                        <tr key={index} className='hover:bg-gray-50'>
                           <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 ">
                             {useFormattedDate(order?.created_at)}
                           </td>
@@ -91,15 +91,15 @@ export default function CustomerOrder() {
                           <td
                             className={`py-2 px-4 border-b text-sm font-medium capitalize ${
                               order?.status === 'Complete'
-                                ? 'bg-green-100 text-green-700'
+                                ? ' text-green-700'
                                 : order?.status === 'pending'
-                                  ? 'bg-yellow-100 text-yellow-700'
+                                  ? ' text-yellow-400'
                                   : order?.status === 'Shipped'
-                                    ? 'bg-sky-100 text-sky-700'
+                                    ? ' text-sky-700'
                                     : order?.status === 'Processing'
-                                      ? 'bg-purple-100 text-purple-700'
-                                      : order?.status === 'Cancelled'
-                                        ? 'bg-red-100 text-red-700'
+                                      ? ' text-purple-700'
+                                      : order?.status === 'Cancel'
+                                        ? ' text-red-700'
                                         : ''
                             }`}
                           >
@@ -120,7 +120,7 @@ export default function CustomerOrder() {
                             <div className="flex items-center space-x-2">
                               <button
                                 onClick={() => openModal(order?.id)}
-                                className="focus:outline-none transition-all duration-100 py-2 px-5 rounded-full bg-[#eab3081a] hover:bg-[#eab308] text-[#eab308] hover:text-lightColor"
+                                className="focus:outline-none transition-all duration-100 py-2 px-6 rounded-full bg-gray-100  hover:bg-gray-500 text-black hover:text-lightColor"
                               >
                                 {/* <FiEye className="text-[12px]" /> */}
                                 View
@@ -137,18 +137,18 @@ export default function CustomerOrder() {
                                   <Tooltip text="You can't cancel this Product">
                                     <button
                                       disabled
-                                      className={`focus:outline-none  transition-all duration-300 p-2 rounded-full bg-[#f43f5e1a] text-[#f43f5e]`}
+                                      className={`focus:outline-none  transition-all duration-300 p-2  px-6  rounded-full bg-[#f43f5e1a] text-[#f43f5e]`}
                                     >
-                                      Cancel Order
+                                      Cancel
                                     </button>
                                   </Tooltip>
                                 </>
                               ) : (
                                 <>
                                   <button
-                                    className={`focus:outline-none transition-all duration-300 p-2 rounded-full bg-[#f43f5e1a] text-[#f43f5e] hover:bg-[#f43f5e] hover:text-lightColor`}
+                                    className={`focus:outline-none transition-all duration-300 p-2 px-6 rounded-full bg-[#f43f5e1a] text-[#f43f5e] hover:bg-[#f43f5e] hover:text-lightColor`}
                                   >
-                                    Cancel Order
+                                    Cancel 
                                   </button>
                                 </>
                               )}
